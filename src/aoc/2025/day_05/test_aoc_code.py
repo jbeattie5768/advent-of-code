ef56378a54285769175167d5c2d03040e5463520
jbeattie5768 <jbeattie5768@gmail.com>
2025-12-03 02:39:56 +0000

    Add: Folders and boilerplate code for 2025 12 days created

---
+++ src/aoc/2025/day_05/test_aoc_code.py
@@ -0,0 +1,18 @@
+# noqa: INP001
+"""AoC 2025 Day 5 test file.
+
+uv run ./src/aoc/2025/day_05/aoc_code.py
+uv run pytest ./src/aoc/2025/day_05/ -rs -v
+uv run mypy --strict ./src/aoc/2025/day_05/
+"""
+
+from .aoc_code import part1, part2  # type: ignore[import-not-found]  # mypy
+
+example_data = [""]  # Add example data here for development
+
+def test_part1() -> None:
+    assert part1(example_data) == 0
+
+def test_part2() -> None:
+    assert part2(example_data) == 0
+
---
+++ src/aoc/2025/day_11/aoc_code.py
@@ -0,0 +1,33 @@
+# noqa: INP001
+"""AoC 2025 Day 11 test file.
+
+uv run ./src/aoc/2025/day_11/aoc_code.py
+uv run pytest ./src/aoc/2025/day_11/ -rs -v
+uv run mypy --strict ./src/aoc/2025/day_11/
+"""
+
+from pathlib import Path
+
+example_data = [""]  # Add example data here for development
+
+def read_data() -> list[str]:
+    file_dir = Path(__file__).parent
+    return Path(f"{file_dir}input.txt").open("r").read().splitlines()
+
+def part1(data: list[str]) -> int:
+    return 0
+
+def part2(data: list[str]) -> int:
+    return 0
+
+if __name__ == "__main__":
+    res_part1 = part1(example_data)
+    #res_part2 = part2(example_data)
+    # res_part1 = part1(read_data())
+    # res_part2 = part2(read_data())
+
+    # Not tried these....yet
+    # data = get_data(1, 2025)  # aocd to get data
+    # aocd.submit(res_part1, part="a", day=1, year=2025)
+    # aocd.submit(res_part2, part="b", day=1, year=2025)
+
